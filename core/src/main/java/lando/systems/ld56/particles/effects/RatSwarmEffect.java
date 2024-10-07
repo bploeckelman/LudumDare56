8ac387de5af2e6fd959d107ee56a190b7f3cdd4c
Jeffrey Hwang <jjhwang@wisc.edu>
2024-10-07 18:48:59 -0500

    swarm effect update

--- core/src/main/java/lando/systems/ld56/particles/effects/RatSwarmEffect.java
+++ core/src/main/java/lando/systems/ld56/particles/effects/RatSwarmEffect.java
@@ -1,10 +1,15 @@
 package lando.systems.ld56.particles.effects;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
+import lando.systems.ld56.Config;
 import lando.systems.ld56.assets.Anims;
 import lando.systems.ld56.particles.Particle;
 import lando.systems.ld56.particles.ParticleManager;
+
+import java.util.ArrayList;
 
 public class RatSwarmEffect extends ParticleEffect {
 
@@ -12,35 +17,30 @@ public class RatSwarmEffect extends ParticleEffect {
         super(particleManager);
     }
 
-    public static class Params implements ParticleEffectParams {
-        public float startX;
-        public float startY;
-        public Params(float x, float y) {
-            startX = x;
-            startY = y;
-        }
-    }
+
 
     @Override
     public void spawn(ParticleEffectParams parameters) {
-        var params = (RatSwarmEffect.Params) parameters;
         var layer = particleManager.activeParticles.get(ParticleManager.Layer.FOREGROUND);
         var pool = particleManager.particlePool;
-        var angle = 0f;
-        var amount = 10;
-        var animation = Anims.get(Anims.Type.RAT_WALK);
+        var amount = 50;
+        var list = new ArrayList<Animation<TextureRegion>>();
+        list.add(Anims.get(Anims.Type.RAT_WALK));
+        list.add(Anims.get(Anims.Type.ANT_WALK));
+        list.add(Anims.get(Anims.Type.WORM_WALK));
+        list.add(Anims.get(Anims.Type.PHAGE_WALK));
 
         for (int i = 0; i < amount; i++) {
-            var startSize = MathUtils.random(5f, 20f);
-            var ttl = MathUtils.random(.5f, 1f);
-            var startPosY = MathUtils.random(params.startY - 50, params.startY + 50);
-            var velocity = MathUtils.random(70f, 100f);
+            var startSize = MathUtils.random(30f, 50f);
+            var ttl = MathUtils.random(5f, 10f);
+            var animation = list.get(MathUtils.random(list.size() - 1));
+            var velocity = 90f;
             layer.add(Particle.initializer(pool.obtain())
                 .animation(animation)
-                .startPos(params.startX - 50f, startPosY)
-                .velocity(MathUtils.cosDeg(angle) * velocity, MathUtils.sinDeg(angle) * velocity)
+                .startPos(- 10f, MathUtils.random(0f, 200f))
+                .targetPos(Config.Screen.window_width + 10f, MathUtils.random(0f, 200f))
                 .startColor(Color.WHITE)
-                .startSize(startSize)
+                .startSize(animation.getKeyFrame(0).getRegionWidth(), animation.getKeyFrame(0).getRegionHeight())
                 .timeToLive(ttl)
                 .init()
             );
